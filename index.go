86fecf8b556b2ce40fc7f9a98cb165c1698b1afc
coder_abhi <abhi.kamble.3987@gmail.com>
2022-01-15 10:27:00 +0530

    Flag funtion added

--- index.go
+++ index.go
@@ -1,1 +1,21 @@
 package main
+
+import "flag"
+
+func main() {
+
+	// Flag
+
+	//Flag Name , Default Value   , Usage
+	timeLimit := flag.Int("limit", 15, "Time limit for Quiz")
+	flag.Parse()
+
+	_ = timeLimit
+
+	/* Use of Flag :-
+	- It is used to give input to our program from terminal at the time of execution
+	- If no input is given it will consider default value as input
+	- But It will show 3rd parameter usage when we run it as help
+	  like - go run index.go -h (or) -help
+	*/
+}
